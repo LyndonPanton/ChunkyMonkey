4ec0686893a014e2941b82c30e98ad51633a9be8
Lyndon Panton <LyndonPanton@gmail.com>
2019-06-29 18:23:19 +0100

    Added key down event listener to chevron
    activates focus is on the chevron and the enter key is pressed or the spacebar is pressed

--- js/app.js
+++ js/app.js
@@ -4,11 +4,19 @@ window.onload = function(event) {
 	document.getElementById("copyright-year").textContent = (new Date()).getFullYear();
 
 	function toggle(chevron) {
-		
+
 	}
 
 	let chevron = document.getElementsByClassName("fas")[0];
 	chevron.addEventListener("click", function(event) {
 		toggle(this);
+		console.log(1);
+	});
+
+	chevron.addEventListener("keydown", function(event) {
+		if (event.keyCode === 13 || event.keyCode === 32) {
+			toggle(this);
+			console.log(2);
+		}
 	});
 };
